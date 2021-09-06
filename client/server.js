2d8abbd11cc66c2f4fb075210a9ed17a977600ac
VarunAroraCode <varora97@gmail.com>
2021-09-05 20:03:15 -0700

    new depend...

--- client/server.js
+++ client/server.js
@@ -4,12 +4,12 @@ const app = express();
 var cors = require('cors');
 const port = process.env.PORT || 5000; 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('public'));
     app.get('*', (req,res) => {
-        req.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 
 }
